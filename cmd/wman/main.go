2977780f8406243799a6be21b8cb23ce24600354
Ken Sipe <kensipe@gmail.com>
2020-09-21 09:26:21 -0500

    adding string reverse with tests
    Signed-off-by: Ken Sipe <kensipe@gmail.com>

--- cmd/wman/main.go
+++ cmd/wman/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/codementor/go-mistakes/pkg/string"
 )
 
 func main() {
-	fmt.Println("hello")
+	name := os.Args[1]
+	fmt.Printf("hello %s, your name backward is %q", name, string.Reverse(name))
 }
